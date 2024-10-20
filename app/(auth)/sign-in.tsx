eaf90ebf4934876973c09fc80bf2e85137ab23dd
Ezekiel <zikkyakpan@gmail.com>
2024-10-20 06:50:12 +0100

    ✨ feat: store user registration details with PostgreSQL on Neon

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,4 +1,4 @@
-import { Image, Text, View } from 'react-native';
+import { Alert, Image, Text, View } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
 import { images, icons } from '@/constants';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -9,6 +9,7 @@ import { Link, router } from 'expo-router';
 import OAuth from '@/components/OAuth';
 import { useSignUp } from '@clerk/clerk-expo';
 import ReactNativeModal from 'react-native-modal';
+import { fetchAPI } from '@/lib/fetch';
 
 const SignUp = () => {
   const { isLoaded, signUp, setActive } = useSignUp();
@@ -59,7 +60,14 @@ const SignUp = () => {
       });
 
       if (completeSignUp.status === 'complete') {
-        //TODO: Create a database user!
+        await fetchAPI('/(api)/user', {
+          method: 'POST',
+          body: JSON.stringify({
+            name: form.name,
+            email: form.email,
+            clerkId: completeSignUp.createdUserId,
+          }),
+        });
 
         await setActive({ session: completeSignUp.createdSessionId });
         setVerification({ ...verification, state: 'success' });
@@ -208,7 +216,7 @@ const SignUp = () => {
               title="Browse Home"
               onPress={() => {
                 setShowSuccessModal(false);
-                router.push('./(root)/(tabs)/home');
+                router.push('/(root)/(tabs)/home');
               }}
               className="mt-5"
             />
