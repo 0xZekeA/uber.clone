4df03cb9a9d0df2e458fef9fc080d8f292ee3b59
Ezekiel <zikkyakpan@gmail.com>
2024-10-20 19:22:48 +0100

    ✨ feat: develop Rides component and integrate into home page

--- app/(root)/(tabs)/home.tsx
+++ app/(root)/(tabs)/home.tsx
@@ -1,14 +1,27 @@
 import { SignedIn, SignedOut, useUser } from '@clerk/clerk-expo';
 import { Link } from 'expo-router';
-import { Text, View } from 'react-native';
+import {
+  ActivityIndicator,
+  FlatList,
+  Image,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { recentRides } from '@/constants/Rides';
+import RideCard from '@/components/RideCard';
+import { icons, images } from '@/constants';
 
 export default function Page() {
   const { user } = useUser();
+  const loading = true;
+
+  const handleSignOut = () => {};
 
   return (
     <SafeAreaView>
-      <SignedIn>
+      {/* <SignedIn>
         <Text>Hello {user?.emailAddresses[0].emailAddress}</Text>
       </SignedIn>
       <SignedOut>
@@ -18,7 +31,56 @@ export default function Page() {
         <Link href="/sign-up">
           <Text>Sign Up</Text>
         </Link>
-      </SignedOut>
+      </SignedOut> */}
+      <View className="bg-general-500">
+        <FlatList
+          data={recentRides?.slice(0, 5)}
+          renderItem={({ item }) => <RideCard ride={item} />}
+          className="px-5"
+          keyboardShouldPersistTaps="handled"
+          contentContainerStyle={{
+            paddingBottom: 100,
+          }}
+          ListEmptyComponent={() => (
+            <View className="flex flex-col items-center justify-center">
+              {!loading ? (
+                <>
+                  <Image
+                    source={images.noResult}
+                    className="w-40 h-40"
+                    alt="No recent rides found"
+                    resizeMode="contain"
+                  />
+                  <Text className="text-sm">No recent rides found</Text>
+                </>
+              ) : (
+                <>
+                  <ActivityIndicator size="small" color="#000" />
+                  <Text>Loading..</Text>
+                </>
+              )}
+            </View>
+          )}
+          ListHeaderComponent={() => (
+            <>
+              <View className="flex flex-row items-center justify-between my-5">
+                <Text className="text-2xl font-JakartaBold capitalize">
+                  Welcome,{' '}
+                  {user?.firstName ||
+                    user?.emailAddresses[0].emailAddress.split('@')[0]}
+                  👋
+                </Text>
+                <TouchableOpacity
+                  onPress={handleSignOut}
+                  className="justify-center items-center w-10 h-10 rounded-full bg-white"
+                >
+                  <Image source={icons.out} className="w-4 h-4" />
+                </TouchableOpacity>
+              </View>
+            </>
+          )}
+        />
+      </View>
     </SafeAreaView>
   );
 }
