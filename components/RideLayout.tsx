acfd1ac579ee857066e66c49681d379083f195fc
Ezekiel <zikkyakpan@gmail.com>
2024-10-21 15:13:03 +0100

    🚗 feat: build find-ride screen for crosschecking destination and pickup location

---
+++ components/RideLayout.tsx
@@ -0,0 +1,99 @@
+import { icons } from '@/constants';
+import { router } from 'expo-router';
+import {
+  Image,
+  Keyboard,
+  Platform,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import Map from '@/components/Map';
+import BottomSheet, { BottomSheetView } from '@gorhom/bottom-sheet';
+import { useEffect, useRef, useState } from 'react';
+
+const RideLayout = ({
+  title,
+  children,
+  snapPoints,
+}: {
+  title: string;
+  children: React.ReactNode;
+  snapPoints?: string[];
+}) => {
+  const bottomSheetRef = useRef<BottomSheet>(null);
+
+  const [snapPoint, setSnapPoint] = useState(['40%', '85%']);
+  const [keyboardVisible, setKeyboardVisible] = useState(false);
+
+  useEffect(() => {
+    const keyboardDidShowListener = Keyboard.addListener(
+      Platform.OS === 'ios' ? 'keyboardWillShow' : 'keyboardDidShow',
+      () => setKeyboardVisible(true)
+    );
+    const keyboardDidHideListener = Keyboard.addListener(
+      Platform.OS === 'ios' ? 'keyboardWillHide' : 'keyboardDidHide',
+      () => setKeyboardVisible(false)
+    );
+
+    return () => {
+      keyboardDidHideListener.remove();
+      keyboardDidShowListener.remove();
+    };
+  }, []);
+
+  useEffect(() => {
+    if (keyboardVisible) {
+      bottomSheetRef.current?.snapToIndex(1);
+    } else {
+      bottomSheetRef.current?.snapToIndex(0);
+    }
+  }, [keyboardVisible]);
+
+  const handleSheetChanges = (index: number) => {
+    if (index === 0) {
+      const currentSnapPoint = snapPoint[0];
+      if (parseInt(currentSnapPoint) < 70 && keyboardVisible) {
+        Keyboard.dismiss();
+      }
+    }
+  };
+
+  return (
+    <GestureHandlerRootView>
+      <View className="flex-1 bg-white">
+        <View className="flex flex-col h-screen bg-blue-500">
+          <View className="flex flex-row absolute z-10 top-16 items-center justify-start px-5">
+            <TouchableOpacity onPress={() => router.back()}>
+              <View className="w-10 h-10 bg-white rounded-full items-center justify-center">
+                <Image
+                  source={icons.backArrow}
+                  resizeMode="contain"
+                  className="w-6 h-6"
+                />
+              </View>
+            </TouchableOpacity>
+            <Text className="text-xl font-JakartaBold ml-5">
+              {title || 'Go Back'}
+            </Text>
+          </View>
+          <Map />
+        </View>
+        <BottomSheet
+          keyboardBehavior="extend"
+          ref={bottomSheetRef}
+          snapPoints={snapPoint}
+          index={0}
+          onChange={handleSheetChanges}
+        >
+          <BottomSheetView style={{ flex: 1, padding: 20 }}>
+            {children}
+          </BottomSheetView>
+        </BottomSheet>
+      </View>
+    </GestureHandlerRootView>
+  );
+};
+
+export default RideLayout;
