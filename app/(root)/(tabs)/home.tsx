275418780333d750da9e3bb1fc8327beae761da2
Ezekiel <zikkyakpan@gmail.com>
2024-10-21 11:17:31 +0100

    ✨ feat: add Map component with user and hard-coded driver markers

--- app/(root)/(tabs)/home.tsx
+++ app/(root)/(tabs)/home.tsx
@@ -8,30 +8,54 @@ import {
   TouchableOpacity,
   View,
 } from 'react-native';
+import { useEffect, useState } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { recentRides } from '@/constants/Rides';
 import RideCard from '@/components/RideCard';
 import { icons, images } from '@/constants';
+import GoogleTextInput from '@/components/GoogleTextInput';
+import Map from '@/components/Map';
+import { useLocationStore } from '@/store';
+import * as Location from 'expo-location';
 
 export default function Page() {
+  const { setUserLocation, setDestinationLocation } = useLocationStore();
   const { user } = useUser();
   const loading = true;
 
+  const [hasPermissions, setHasPermissions] = useState(false);
+
   const handleSignOut = () => {};
+  const handleDestinationPress = () => {};
+
+  useEffect(() => {
+    const requestLoctaion = async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+
+      if (status !== 'granted') {
+        setHasPermissions(false);
+        return;
+      }
+      let location = await Location.getCurrentPositionAsync();
+
+      const address = await Location.reverseGeocodeAsync({
+        latitude: location.coords?.latitude!,
+        longitude: location.coords?.longitude!,
+      });
+
+      setUserLocation({
+        // latitude: location.coords.latitude,
+        // longitude: location.coords.longitude,
+        latitude: 37.7749,
+        longitude: -122.4194,
+        address: `${address[0].name}, ${address[0].region}`,
+      });
+    };
+    requestLoctaion();
+  }, []);
 
   return (
     <SafeAreaView>
-      {/* <SignedIn>
-        <Text>Hello {user?.emailAddresses[0].emailAddress}</Text>
-      </SignedIn>
-      <SignedOut>
-        <Link href="/sign-in">
-          <Text>Sign In</Text>
-        </Link>
-        <Link href="/sign-up">
-          <Text>Sign Up</Text>
-        </Link>
-      </SignedOut> */}
       <View className="bg-general-500">
         <FlatList
           data={recentRides?.slice(0, 5)}
@@ -65,7 +89,7 @@ export default function Page() {
             <>
               <View className="flex flex-row items-center justify-between my-5">
                 <Text className="text-2xl font-JakartaBold capitalize">
-                  Welcome,{' '}
+                  Welcome{' '}
                   {user?.firstName ||
                     user?.emailAddresses[0].emailAddress.split('@')[0]}
                   👋
@@ -77,6 +101,22 @@ export default function Page() {
                   <Image source={icons.out} className="w-4 h-4" />
                 </TouchableOpacity>
               </View>
+              <GoogleTextInput
+                icon={icons.search}
+                containerStyle="bg-white shadow-md shadow-neutral-300"
+                handlePress={handleDestinationPress}
+              />
+              <>
+                <Text className="text-xl font-JakartaBold mt-5 mb-3">
+                  Your current Location
+                </Text>
+                <View className="flex flex-row items-center bg-transparent h-[300px]">
+                  <Map />
+                </View>
+              </>
+              <Text className="text-xl font-JakartaBold mt-5 mb-3">
+                Recent Rides
+              </Text>
             </>
           )}
         />
